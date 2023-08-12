f7d9cabf05b25aee7470cf3a86f873eba7ffe165
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-08-12 22:25:46 +0530

    Added docker compose

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module stargazer39/port-forwarder
 
 go 1.20
-
-require (
-	github.com/google/uuid v1.3.0
-	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb
-)
--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"log"
 	"os"
-
-	"golang.org/x/exp/slices"
+	"strings"
 )
 
 var (
@@ -16,13 +15,13 @@ func main() {
 	Debug = log.New(os.Stderr, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Info = log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-	proxyServerPort := os.Getenv("PROXY_SERVER_PORT")
+	proxyServerPort := os.Getenv("PROXY_SERVER_ADDRESS")
 	forwardPort := os.Getenv("PROXY_FORWARD_PORT")
 
 	proxyPort := os.Getenv("PROXY_CLIENT_PORT")
 	listenPort := os.Getenv("PROXY_LISTEN_PORT")
 
-	if slices.Contains(os.Args, "server-client") {
+	if strings.Contains(strings.Join(os.Args, " "), "server-client") {
 		Info.Println("Starting in server-client mode")
 		ServerClient(proxyServerPort, forwardPort)
 		return
